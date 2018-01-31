65bdbf7265b3286826be42464aa7c94ca6cb5511
Triqueneaux Calvin <calvin8313@hotmail.fr>
2018-01-31 13:17:56 +0100

    project finished

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,47 @@
+class GossipsController < ApplicationController
+  def index
+  	@gossips = Gossip.all
+  end
+
+  def new
+  	@gossip = Gossip.new
+  end
+
+  def create
+  	@gossip = Gossip.new(gossip_params)
+  	if @gossip.save
+  	  redirect_to gossip_path(@gossip)
+  	else
+  	  render 'new'
+  	end
+  end
+
+  def show
+  	@gossip = Gossip.find(params[:id])
+  end
+
+  def edit
+  	@gossip = Gossip.find(params[:id])
+  end
+
+  def update
+  	@gossip = Gossip.find(params[:id])
+  	new_gossip = gossip_params
+  	if @gossip.update(new_gossip)
+  	  redirect_to gossip_path(@gossip)
+  	else
+  	  render 'edit'
+  	end
+  end
+
+  def destroy
+  	@gossip = Gossip.find(params[:id])
+  	@gossip.destroy
+  	redirect_to gossips_path
+  end
+
+  private
+  def gossip_params
+  	params.require(:gossip).permit(:anonymous_author, :content)
+  end
+end
